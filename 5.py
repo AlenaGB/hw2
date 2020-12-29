01132598937efd39829614a37690aeb262ab4705
AlenaGB <76430389+AlenaGB@users.noreply.github.com>
2020-12-29 13:08:15 +0300

    Fixad bug in if in 5 task

--- 5.py
+++ 5.py
@@ -1,20 +1,20 @@
 myRait = list(map(int,input('Enter the initial value of the rating: ').split()))
 print(' '.join(map(str,myRait)))
 
-sorRait = sorted(myRait)
-print(' '.join(map(str,sorRait)))
+myRait.sort(reverse = True)
+print(' '.join(map(str,myRait)))
 
 number = int(input('Enter the new value of the rating: '))
 
 while number <=0:
     number = int(input('Enter a natural number: '))
 
-for el in range(len(sorRait)):
-        if number == sorRait[el] or number < sorRait[el]:
-            sorRait.insert(el,number)
+for el in range(len(myRait)):
+        if number == myRait[el] or number > myRait[el]:
+            myRait.insert(el,number)
             break
 else:
-    sorRait.append(number)
+    myRait.append(number)
 
 print("New rating is: ")
-print(' '.join(map(str,sorRait)))
+print(' '.join(map(str,myRait)))
